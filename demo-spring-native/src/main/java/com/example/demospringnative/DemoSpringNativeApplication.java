7492783fc3c8923c55294a3755ff2268f6fe3855
Diego Lirio <83074806+ddamacena@users.noreply.github.com>
2021-06-30 12:22:24 -0300

    First

---
+++ demo-spring-native/src/main/java/com/example/demospringnative/DemoSpringNativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringNativeApplication.class, args);
+	}
+
+}
---
+++ demo-spring-native/src/test/java/com/example/demospringnative/DemoSpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
